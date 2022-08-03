041c9ef5658e7e755abd8ff7c4bb626d2b83de59
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-08-03 18:33:48 +0600

    migration added

---
+++ StudentRegistration/DBContext/Student_DBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using StudentRegistration.Models;
+
+namespace StudentRegistration.DBContext
+{
+    public class Student_DBContext: DbContext
+    {
+        public Student_DBContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<AcademicClass> AcademicClasses { get; set; }
+    }
+}
---
+++ StudentRegistration/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentRegistration.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StudentRegistration.DBContext.Student_DBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(StudentRegistration.DBContext.Student_DBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
